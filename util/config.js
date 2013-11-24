2845320df7c914ea7e26c3a371db784e05a3a528
presentaciones <presentaciones@asus-u32u.(none)>
2013-11-24 23:11:16 +0100

    add stage environment

--- util/config.js
+++ util/config.js
@@ -1,3 +1,6 @@
+
+madridjswebbackend
+
 var development = {
 
     environment             : 'development',
@@ -5,11 +8,18 @@ var development = {
     urlMongoDB              : 'mongodb://localhost/madridjs-web-backend-v01'
 }
 
+var stage = {
+
+    environment             : 'stage',
+    port                    : 5000,
+    urlMongoDB              : 'mongodb://madridjswebbackend:ds057548@ds057548.mongolab.com:57548/madridjswebbackend'
+}
+
 var production = {
 
     environment             : 'production',
     port                    : 5000,
-    urlMongoDB              : 'mongodb://madridjswebbackend:Madr1djs2013@ds057548.mongolab.com:57548/madridjswebbackend'
+    urlMongoDB              : 'mongodb://madridjswebbackend:ds057548@ds057548.mongolab.com:57548/madridjswebbackend'
 }
 
 var env = process.env.NODE_ENV;
@@ -17,6 +27,9 @@ var env = process.env.NODE_ENV;
 switch (env) {
     case 'development':
         exports.properties = development;
+        break;
+    case 'stage':
+        exports.properties = stage;
         break;
     default:
         exports.properties = production;
